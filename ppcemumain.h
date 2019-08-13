eeb1e46889100756a06141630bf1e3dcda8978e6
Maxim Poliakovski <maximumspatium@googlemail.com>
2019-08-13 15:59:11 +0200

    Fix dispatch map for sync opcode.

--- ppcemumain.h
+++ ppcemumain.h
@@ -669,7 +669,7 @@ static std::map<uint16_t, PPCOpcode> SubOpcode31Grabber=
      {1068, &ppc_lwbrx},     {1070, &ppc_lfsx},       {1072, &ppc_srw},       {1073, &ppc_srwdot},
      {1074, &power_rrib},    {1075, &power_rribdot},  {1082, &power_maskir},  {1083, &power_maskirdot},
      {1104, &ppc_subfo},     {1105, &ppc_subfodot},   {1134, &ppc_lfsux},     {1190, &ppc_mfsr},
-     {1194, &ppc_lswi},      {1196, &ppc_isync},      {1232, &ppc_nego},      {1233, &ppc_negodot},
+     {1194, &ppc_lswi},      {1196, &ppc_sync},       {1232, &ppc_nego},      {1233, &ppc_negodot},
      {1238, &power_mulo},    {1239, &power_mulodot},
      {1300, &ppc_addeo},     {1301, &ppc_addeodot},   {1318, &ppc_mfsrin},    {1322, &ppc_stswx},
      {1324, &ppc_stwbrx},    {1328, &power_srq},      {1329, &power_srqdot},  {1330, &power_sre},
