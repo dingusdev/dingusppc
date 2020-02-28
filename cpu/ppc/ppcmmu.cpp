354409812ea951f68bed4cf96af9183e1b91ec50
dingusdev <kataetheweirdo@gmail.com>
2020-02-28 09:04:28 -0700

    Updating files with proper licensing header

--- cpu/ppc/ppcmmu.cpp
+++ cpu/ppc/ppcmmu.cpp
@@ -1,9 +1,23 @@
-//DingusPPC
-//Written by divingkatae and maximum
-//(c)2018-20 (theweirdo)     spatium
-//Please ask for permission
-//if you want to distribute this.
-//(divingkatae#1017 or powermax#2286 on Discord)
+/*
+DingusPPC - The Experimental PowerPC Macintosh emulator
+Copyright (C) 2018-20 divingkatae and maximum
+                      (theweirdo)     spatium
+
+(Contact divingkatae#1017 or powermax#2286 on Discord for more info)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 /** @file PowerPC Memory management unit emulation. */
 
