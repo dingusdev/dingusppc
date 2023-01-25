bcd443779a6e3ba2047ff89750e467ce5b4e96ba
Maxim Poliakovski <maximumspatium@googlemail.com>
2023-01-25 20:56:51 +0100

    machinegossamer: add internal SCSI bus.

--- machines/machinegossamer.cpp
+++ machines/machinegossamer.cpp
@@ -28,12 +28,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <devices/common/i2c/athens.h>
 #include <devices/common/i2c/i2cprom.h>
 #include <devices/common/machineid.h>
-#include <devices/floppy/floppyimg.h>
+#include <devices/common/scsi/scsi.h>
 #include <devices/ioctrl/macio.h>
 #include <devices/memctrl/mpc106.h>
 #include <devices/memctrl/spdram.h>
-#include <devices/sound/soundserver.h>
-#include <devices/video/atirage.h>
 #include <loguru.hpp>
 #include <machines/machinebase.h>
 #include <machines/machinefactory.h>
@@ -125,6 +123,8 @@ int initialize_gossamer(std::string& id)
         16, dynamic_cast<PCIDevice*>(gMachineObj->get_comp_by_name("Heathrow")));
     grackle_obj->pci_register_device(
         18, dynamic_cast<PCIDevice*>(gMachineObj->get_comp_by_name(id == "pmg3twr" ? "AtiRagePro" : "AtiRageGT")));
+
+    gMachineObj->add_device("SCSI0", std::unique_ptr<ScsiBus>(new ScsiBus()));
 
     // add Athens clock generator device and register it with the I2C host
     gMachineObj->add_device("Athens", std::unique_ptr<AthensClocks>(new AthensClocks(0x28)));
