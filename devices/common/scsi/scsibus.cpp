6f247f64cd69bdc1a0d87ec0b4490edb93a953a4
Maxim Poliakovski <maximumspatium@googlemail.com>
2023-05-30 19:51:07 +0200

    ScsiBus: add missing include.

--- devices/common/scsi/scsibus.cpp
+++ devices/common/scsi/scsibus.cpp
@@ -27,6 +27,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <loguru.hpp>
 
 #include <cinttypes>
+#include <memory>
 
 ScsiBus::ScsiBus(const std::string name)
 {
