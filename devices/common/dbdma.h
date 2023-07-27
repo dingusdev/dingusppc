4960861056eb84d9405536354ddd4120de3fa01f
joevt <joevt@shaw.ca>
2023-07-27 11:39:19 -0700

    dbdma: Add missing flags, fields, comments.

--- devices/common/dbdma.h
+++ devices/common/dbdma.h
@@ -39,15 +39,37 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 enum DMAReg : uint32_t {
     CH_CTRL         = 0,
     CH_STAT         = 4,
+    CMD_PTR_HI      = 8, // not implemented
     CMD_PTR_LO      = 12,
     INT_SELECT      = 16,
     BRANCH_SELECT   = 20,
+    WAIT_SELECT     = 24,
+//  TANSFER_MODES   = 28,
+//  DATA_2_PTR_HI   = 32, // not implemented
+//  DATA_2_PTR_LO   = 36,
+//  RESERVED_1      = 40,
+//  ADDRESS_HI      = 44,
+//  RESERVED_2_0    = 48,
+//  RESERVED_2_1    = 52,
+//  RESERVED_2_2    = 56,
+//  RESERVED_2_3    = 60,
+//  UNIMPLEMENTED   = 64,
+//  UNDEFINED       = 128,
 };
 
 /** Channel Status bits (DBDMA spec, 5.5.3) */
 enum : uint16_t {
-    CH_STAT_ACTIVE = 0x400,
-    CH_STAT_DEAD   = 0x800,
+//  CH_STAT_S0     = 0x0001
+//  CH_STAT_S1     = 0x0002,
+//  CH_STAT_S2     = 0x0004,
+//  CH_STAT_S3     = 0x0008,
+//  CH_STAT_S4     = 0x0010,
+//  CH_STAT_S5     = 0x0020,
+//  CH_STAT_S6     = 0x0040,
+//  CH_STAT_S7     = 0x0080,
+//  CH_STAT_BT     = 0x0100,
+    CH_STAT_ACTIVE = 0x0400,
+    CH_STAT_DEAD   = 0x0800,
     CH_STAT_WAKE   = 0x1000,
     CH_STAT_FLUSH  = 0x2000,
     CH_STAT_PAUSE  = 0x4000,
@@ -57,8 +79,8 @@ enum : uint16_t {
 /** DBDMA command (DBDMA spec, 5.6.1) - all fields are little-endian! */
 typedef struct DMACmd {
     uint16_t    req_count;
-    uint8_t     cmd_bits;
-    uint8_t     cmd_key;
+    uint8_t     cmd_bits; // wait: & 3, branch: & 0xC, interrupt: & 0x30, reserved: & 0xc0
+    uint8_t     cmd_key; // key: & 7, reserved: & 8, cmd: >> 4
     uint32_t    address;
     uint32_t    cmd_arg;
     uint16_t    res_count;
@@ -118,6 +140,7 @@ private:
     uint8_t* queue_data     = 0;
     uint32_t int_select     = 0;
     uint32_t branch_select  = 0;
+    uint32_t wait_select    = 0;
 
     bool     cmd_in_progress = false;
     uint8_t  cur_cmd;
