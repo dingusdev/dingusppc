f1abb66f9a1be616a2a47cb41ee8776e4a2ec14b
joevt <joevt@shaw.ca>
2024-03-29 20:47:25 -0700

    atahd: Add STANDBY_IMMEDIATE.

--- devices/common/ata/atahd.cpp
+++ devices/common/ata/atahd.cpp
@@ -164,6 +164,11 @@ int AtaHardDisk::perform_command() {
         this->r_status &= ~BSY;
         this->update_intrq(1);
         break;
+    case STANDBY_IMMEDIATE_E0:
+        LOG_F(INFO, "%s: STANDBY_IMMEDIATE_E0", this->name.c_str());
+        this->r_status &= ~BSY;
+        this->update_intrq(1);
+        break;
     default:
         LOG_F(ERROR, "%s: unknown ATA command 0x%x", this->name.c_str(), this->r_command);
         this->r_status &= ~BSY;
