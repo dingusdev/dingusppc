630755f9e5a7686e8c4e3d916f84d23833a290b2
Rairii <2650838+Wack0@users.noreply.github.com>
2024-08-23 23:34:54 -0700

    atabasedevice: Include timermanager.

--- devices/common/ata/atabasedevice.cpp
+++ devices/common/ata/atabasedevice.cpp
@@ -27,6 +27,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <loguru.hpp>
 
 #include <cinttypes>
+#include <core/timermanager.h>
 
 using namespace ata_interface;
 
