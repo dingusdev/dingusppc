470f4a115ad98ac7e44d2b9fee290c82d140a857
joevt <joevt@shaw.ca>
2025-03-10 14:34:16 -0700

    debugger: Fix fregs help alignment.

--- debugger/debugger.cpp
+++ debugger/debugger.cpp
@@ -78,7 +78,7 @@ static void show_help() {
     cout << "  until X      -- execute until address X is reached" << endl;
     cout << "  go           -- exit debugger and continue emulator execution" << endl;
     cout << "  regs         -- dump content of the GRPs" << endl;
-    cout << "  fregs         -- dump content of the FPRs" << endl;
+    cout << "  fregs        -- dump content of the FPRs" << endl;
     cout << "  mregs        -- dump content of the MMU registers" << endl;
     cout << "  set R=X      -- assign value X to register R" << endl;
     cout << "                  if R=loglevel, set the internal" << endl;
