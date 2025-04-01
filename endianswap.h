3bab9b6f520f0e09678f49e7248294c8c41fae2c
joevt <joevt@shaw.ca>
2025-04-01 05:02:38 -0700

    endianswap: Convert tabs to spaces.

--- endianswap.h
+++ endianswap.h
@@ -69,26 +69,26 @@ inline std::uint16_t byteswap_16_impl(std::uint16_t x)
 
 inline std::uint32_t byteswap_32_impl(std::uint32_t x)
 {
-  	return (
-  		  ((x & UINT32_C(0x000000FF)) << 24)
-  		| ((x & UINT32_C(0x0000FF00)) << 8)
-  		| ((x & UINT32_C(0x00FF0000)) >> 8)
-  		| ((x & UINT32_C(0xFF000000)) >> 24)
-  		);
+    return (
+          ((x & UINT32_C(0x000000FF)) << 24)
+        | ((x & UINT32_C(0x0000FF00)) << 8)
+        | ((x & UINT32_C(0x00FF0000)) >> 8)
+        | ((x & UINT32_C(0xFF000000)) >> 24)
+        );
 }
 
 inline std::uint64_t byteswap_64_impl(std::uint64_t x)
 {
-	return (
-		  ((x & UINT64_C(0x00000000000000FF)) << 56)
-		| ((x & UINT64_C(0x000000000000FF00)) << 40)
-		| ((x & UINT64_C(0x0000000000FF0000)) << 24)
-		| ((x & UINT64_C(0x00000000FF000000)) << 8)
-		| ((x & UINT64_C(0x000000FF00000000)) >> 8)
-		| ((x & UINT64_C(0x0000FF0000000000)) >> 24)
-		| ((x & UINT64_C(0x00FF000000000000)) >> 40)
-		| ((x & UINT64_C(0xFF00000000000000)) >> 56)
-		);
+    return (
+          ((x & UINT64_C(0x00000000000000FF)) << 56)
+        | ((x & UINT64_C(0x000000000000FF00)) << 40)
+        | ((x & UINT64_C(0x0000000000FF0000)) << 24)
+        | ((x & UINT64_C(0x00000000FF000000)) << 8)
+        | ((x & UINT64_C(0x000000FF00000000)) >> 8)
+        | ((x & UINT64_C(0x0000FF0000000000)) >> 24)
+        | ((x & UINT64_C(0x00FF000000000000)) >> 40)
+        | ((x & UINT64_C(0xFF00000000000000)) >> 56)
+        );
 }
 
 #   define BYTESWAP_16(x) (byteswap_16_impl(x))
