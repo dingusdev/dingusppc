8b983ea103f3356da729e8cee620ec8bebab97fb
joevt <joevt@shaw.ca>
2026-09-04 02:57:45 -0700

    sc53c94: Update IRQ after RESET_DEVICE.
    Everywhere that int_status is changed, update_irq must be called to
    update irq and status.

--- devices/common/scsi/sc53c94.cpp
+++ devices/common/scsi/sc53c94.cpp
@@ -314,6 +314,7 @@ void Sc53C94::exec_command()
     case CMD_RESET_DEVICE:
         reset_device();
         this->on_reset = true; // block the command register
+        this->update_irq();
         return;
     case CMD_RESET_BUS:
         LOG_F(INFO, "%s: resetting SCSI bus...", this->name.c_str());
